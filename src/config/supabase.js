e59b6f2443feb6c3edbac1f58e8016bcee016919
amorales1989 <valentina.morales.paratore@gmail.com>
2025-09-21 22:19:56 -0300

    prueba env

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -5,8 +5,8 @@ if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
   throw new Error('Faltan variables de entorno de Supabase');
 }
 
-const supabaseUrl = 'https://wnmxgjrjrckwtyttidkw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndubXhnanJqcmNrd3R5dHRpZGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxOTU3MzcsImV4cCI6MjA1Mzc3MTczN30.uhtHBW6t8t6ofL2GLvnBy5Gr44cbH-jfp-jKxaXi8Oo';
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 // Crear cliente de Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
